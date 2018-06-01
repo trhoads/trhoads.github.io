40474dadc21c659499f8f30392cea6eec5cea82f
trhoads <35459520+trhoads@users.noreply.github.com>
2018-06-01 14:42:50 -0400

    Worked on Week 6

---
+++ cit230/Assignments/lesson5/weathersite/scripts/scripts.js
@@ -0,0 +1,8 @@
+function toggleNavMenu(){
+	var x = document.getElementById("Nav");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
